49308a0f752129c71cd976b97dc59abb8e2e7d41
PratamaAngga <anggashillnoir@gmail.com>
2025-08-04 17:06:08 +0700

    optimize navbar dan banner

--- js/include.js
+++ js/include.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Load Navbar
+  const currentPage = document.body.dataset.page; // 'forum'
+
   fetch("components/navbar.html")
-    .then(res => res.text())
-    .then(data => {
+    .then((res) => res.text())
+    .then((data) => {
       document.getElementById("navbar").innerHTML = data;
+
+      // Setelah navbar dimuat, baru jalankan pengecekan active
+      document.querySelectorAll(".nav-link").forEach((link) => {
+        if (link.dataset.page === currentPage) {
+          link.classList.add("active");
+        }
+      });
+    });
+
+  // Load Banner
+  fetch("components/banner.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("banner").innerHTML = data;
     });
 
   // Load Footer
   fetch("components/footer.html")
-    .then(res => res.text())
-    .then(data => {
+    .then((res) => res.text())
+    .then((data) => {
       document.getElementById("footer").innerHTML = data;
     });
 });
