9673a2324f2adc4d3e717c1f1ad9e88a58cfbb13
PratamaAngga <anggashillnoir@gmail.com>
2025-08-04 19:02:30 +0700

    final navbar dan habus scrollbar

--- js/include.js
+++ js/include.js
@@ -29,3 +29,12 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("footer").innerHTML = data;
     });
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
